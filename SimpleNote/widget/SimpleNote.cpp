53680303b95f260758c7600cea0226ab4f7ea231
ZY <zyzhouyang1@outlook.com>
2021-12-23 16:43:25 +0800

    添加窗口拖拽

--- SimpleNote/widget/SimpleNote.cpp
+++ SimpleNote/widget/SimpleNote.cpp
@@ -1,7 +1,9 @@
 #include "SimpleNote.h"
+#include <qevent.h>
 
-SimpleNote::SimpleNote(QWidget *parent)
+SimpleNote::SimpleNote(QWidget* parent)
     : FramelessWidget(parent)
+    , m_bDrag(false)
 {
     ui.setupUi(this);
     set_style_file(":/SimpleNote/qss/SimpleNote.qss");
@@ -12,6 +14,33 @@ SimpleNote::SimpleNote(QWidget *parent)
 void SimpleNote::slot_close()
 {
     this->close();
+}
+
+void SimpleNote::mousePressEvent(QMouseEvent* event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        if (ui.widget_title->geometry().contains(mapFromGlobal(QCursor::pos())))
+        {
+            m_bDrag = true;
+            m_mousePressPos = event->globalPos();
+            m_windowStartPos = frameGeometry().topLeft();
+        }
+    }
+}
+
+void SimpleNote::mouseReleaseEvent(QMouseEvent* event)
+{
+    m_bDrag = false;
+}
+
+void SimpleNote::mouseMoveEvent(QMouseEvent* event)
+{
+    if (m_bDrag)
+    {
+        QPoint distance = event->globalPos() - m_mousePressPos;
+        move(m_windowStartPos + distance);
+    }
 }
 
 void SimpleNote::connect_all()
--- SimpleNote/widget/SimpleNote.h
+++ SimpleNote/widget/SimpleNote.h
@@ -11,6 +11,11 @@ class SimpleNote : public FramelessWidget
 public:
     SimpleNote(QWidget *parent = Q_NULLPTR);
 
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+
 private Q_SLOTS:
     void slot_close();
     
@@ -19,4 +24,8 @@ private:
 
 private:
     Ui::SimpleNote ui;
+
+    bool m_bDrag;
+    QPoint m_windowStartPos;
+    QPoint m_mousePressPos;
 };
